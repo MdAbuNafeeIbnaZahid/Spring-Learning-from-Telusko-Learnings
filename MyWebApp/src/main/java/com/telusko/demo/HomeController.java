2957103a954b3703a0b059fb8a8a38741c61ba6e
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2018-08-20 21:20:55 +0600

    compelted up to video 9 of Spring Boot tutorial by Telusko Learnings

--- MyWebApp/src/main/java/com/telusko/demo/HomeController.java
+++ MyWebApp/src/main/java/com/telusko/demo/HomeController.java
@@ -1,16 +1,25 @@
 package com.telusko.demo;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("home")
-	public String home()
+	public ModelAndView home(Alien alien)
 	{
-		System.out.println("hi");
-		return "home.jsp";
+		ModelAndView mView = new ModelAndView();
+		mView.addObject("obj", alien);
+		
+		mView.setViewName("home");
+		
+		return mView;
 	}
 }
--- MyWebApp/src/main/webapp/pages/home.jsp
+++ MyWebApp/src/main/webapp/pages/home.jsp
@@ -7,8 +7,7 @@
 <title>Insert title here</title>
 </head>
 <body>
-	I love Java.
-
+	Welcome  ${obj}, ${obj.aname}
 </body>
 </html>
 
