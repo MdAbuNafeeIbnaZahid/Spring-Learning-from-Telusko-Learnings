3bd011483e991a4cf47d799faf5ac6133df32faa
MdAbuNafeeIbnaZahid <1305023.maniz@ugrad.cse.buet.ac.bd>
2018-08-20 15:47:03 +0600

    Created a jsp servlet application and a spring boot application to understand the difference

---
+++ Demo/src/com/telusko/AddServlet.java
@@ -0,0 +1,44 @@
+package com.telusko;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class AddServlet
+ */
+@WebServlet("/add")
+public class AddServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public AddServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		System.out.println(request.getParameter("t1"));
+		int i = Integer.parseInt(request.getParameter("t1"));
+		int j = Integer.parseInt(request.getParameter("t2"));
+		
+		int k = i + j;
+		System.out.println(k);
+		
+		PrintWriter out = response.getWriter();
+		out.println(k);
+		
+	}
+
+	
+}
